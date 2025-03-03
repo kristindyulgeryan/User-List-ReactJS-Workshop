b505ba6f8f90fc232b6c60a9b31d6b6962ab1cf8
Kristin <kristindyulgeryan@abv.bg>
2025-03-03 17:08:56 +0200

    Create user service and get all

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react";
 import Pagination from "./Pagination.jsx";
 import Search from "./Search.jsx";
 import UserListItem from "./UserListItem.jsx";
+import userService from "../services/userService.js";
 
 export default function UserList() {
+  useEffect(() => {
+    userService.getAll().then((result) => {
+      console.log(result);
+    });
+  }, []);
+
   return (
     <>
       <section className="card users-container">
